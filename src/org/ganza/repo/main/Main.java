7dd8865986e67f6e47a48385f2187e7c571050c8
GANZA Mykhailo <hanzenok@gmail.com>
2015-12-25 20:35:50 +0100

    Added javadoc for main package

--- src/org/ganza/repo/main/Main.java
+++ src/org/ganza/repo/main/Main.java
@@ -2,49 +2,36 @@ package org.ganza.repo.main;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
 
 import org.ganza.repo.controller.MenuController;
-import org.ganza.repo.controller.SearchController;
 import org.ganza.repo.view.RepoView;
-import org.ganza.repo.view.SearchView;
 import org.jdom2.JDOMException;
 
 import net.lingala.zip4j.exception.ZipException;
 
-
+/**
+ * Point d'entrée de la programme
+ * @author Ganza Mykhailo
+ */
 public class Main 
 {	
+	/**
+	 * Methode main
+	 * @param args
+	 * @throws ZipException
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 * @throws JDOMException
+	 */
 	public static void main(String[] args) 
 	throws ZipException, FileNotFoundException, IOException, JDOMException
 	{	
-		
+		//la vue principale
 		RepoView repo_view = new RepoView();
-		MenuController mc = new MenuController(repo_view);
 		
-//		EditorView rv = new EditorView();
-		
-//		Repo repo = new Repo();
-//		
-//		
-//		FilterView fv = new FilterView();
-//		
-//		FilterController fc = new FilterController(repo, null, fv);
-//		fv.show(repo.getAcceptedExtensions());
-//		
-//		fv.setFilterController(fc);
-		
-//		ArrayList<String> attrs = new ArrayList<String>();
-//		attrs.add("author"); attrs.add("genre");
-//
-//		
-//		SearchView search_view = new SearchView();
-//		
-//		SearchController search_controller = new SearchController(null, null, search_view);
-//		
-//		search_view.show(attrs);
-//		search_view.setSearchController(search_controller);
-		
+		//controlleur principale
+		@SuppressWarnings("unused")
+		MenuController mc = new MenuController(repo_view); //c'est controlleur lance la model
 	}
 	
 }
